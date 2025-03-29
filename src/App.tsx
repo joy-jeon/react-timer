7a46a27ded8851fa9fcf5b451053ed3156740d3a
joy-jeon <ha0913hyun@hanmail.net>
2025-03-29 14:19:22 +0900

    기본 마크업

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
 export default function App() {
-  return <div>HELLO</div>;
+  return (
+    <div>
+      <h1>Pomodoro</h1>
+      <div>timer number</div>
+      <button>play/pause</button>
+      <div>
+        <div>
+          <span>0/4</span>
+          <span>Round</span>
+        </div>
+        <div>
+          <span>0/12</span>
+          <span>Goal</span>
+        </div>
+      </div>
+    </div>
+  );
 }
